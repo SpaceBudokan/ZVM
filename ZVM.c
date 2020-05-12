730becb662d1e6516fa78e48ab169943282b3adc
SpaceBudokan <jonathan.zikos@gmail.com>
2020-05-12 09:49:20 -0500

    added pchar, pnum, and add functions

--- ZVM.c
+++ ZVM.c
@@ -6,64 +6,149 @@
 
 #define INT uint64_t
 /*The above sets the size of the bytecode words*/
-INT *ramArray;
-INT ramSize = 100;
+#define FUNCS 
+INT programRam[100];
+INT *stackArray;
+INT stackSize = 100;
 INT pc = 0;
 INT sp = -1;
 INT fp = -1;
-void iconst(void)
-{
-}
-
-void load(void)
-{
-}
-
-void store(void)
-{
-}
-
-void add(void)
-{
-}
-  
-void decode(void)
-{
-  void (*jumptable[4])(void) = {iconst, load, store, add};
-  jumptable[ramArray[pc]]();
-}
 
 void push(INT value)
 {
-  if(sp == ramSize - 1){
-    ramSize = ramSize + 100;
-    ramArray = realloc(ramArray, ramSize);
-    if(ramArray == NULL){
-      printf("ERROR: Unable to allocate ramArray!\n");
+  if(sp == stackSize - 1){
+    stackSize = stackSize + 100;
+    stackArray = realloc(stackArray, stackSize);
+    if(stackArray == NULL){
+      printf("ERROR: Unable to allocate stackArray!\n");
       exit(0);
     }
   }
-  ramArray[sp] = value;
   sp++;
+  stackArray[sp] = value;
   return;
 }
 
 void pop(void){
   sp--;
+  return;
 }
+
+
+void noop(void)
+{
+  /* do nothing */
+  return;
+}
+
+void iconst(void)
+{
+  pc++;
+  push(programRam[pc]);
+  return;
+}
+
+void load(void)
+{
+  return;
+}
+
+void store(void)
+{
+  return;
+}
+
+void add(void)
+{
+  stackArray[sp - 1] = stackArray[sp] + stackArray[sp - 1];
+  pop();
+  return;
+}
+void sub(void)
+{
+  return;
+}
+
+void mult(void)
+{
+  return;
+}
+
+void divide(void)
+{
+  return;
+}
+
+void jump(void)
+{
+  return;
+}
+
+void jsubr(void)
+{
+  return;
+}
+
+void ret(void)
+{
+  return;
+}
+
+void beqz(void)
+{
+  return;
+}
+
+void bneqz(void)
+{
+  return;
+}
+
+void pchar(void)
+{
+  printf("%c", (char)stackArray[sp]);
+  pop();
+  return;
+}
+
+void pnum(void)
+{
+  printf("%lu",stackArray[sp]);
+  pop();
+  return;
+}
+
+void decode(void)
+{
+  /*using a jumptable because some compilers aren't too eager to optimize switch-case loops to jumptables */
+  void (*jumptable[15])(void) = {noop, iconst, load, store, add, sub, mult,
+				divide, jump, jsubr, ret, beqz, bneqz, pchar, pnum};
+  jumptable[programRam[pc]]();
+  return;
+}
+
+
     
 int main(int argc, char **argv)
 {
-  ramArray = malloc(sizeof(INT) * ramSize);
-  if(ramArray == NULL){
-    printf("ERROR: Unable to allocate ramArray!\n");
+  stackArray = malloc(sizeof(INT) * stackSize);
+  if(stackArray == NULL){
+    printf("ERROR: Unable to allocate stackArray!\n");
+    return 1;
   }
 
-  printf("Size: %i\n", size);
-
+  programRam[0] = 1;
+  programRam[1] = 1;
+  programRam[2] = 1;
+  programRam[3] = 1;
+  programRam[4] = 4;
+  programRam[5] = 14;
+  while(pc < 6){
+    decode();
+    pc++;
+  }
   
-  
-  free(ramArray);
+  free(stackArray);
   return 0;
 }
 
